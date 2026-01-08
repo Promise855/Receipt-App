77c8ccdb40e4a06eac183411413e037cb1d7a4e8
Promise855 <chinedupromise855@gmail.com>
2026-01-08 14:40:53 +0100

    minor error fix

--- invoice-generator/tailwind.config.js
+++ invoice-generator/tailwind.config.js
@@ -6,7 +6,16 @@ export default {
   ],
   theme: {
     extend: {
-      // Add your customizations here later (colors, fonts, etc.)
+      // ADD THIS SECTION
+      keyframes: {
+        'fade-in': {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        }
+      },
+      animation: {
+        'fade-in': 'fade-in 0.5s ease-out',
+      }
     },
   },
   plugins: [],
